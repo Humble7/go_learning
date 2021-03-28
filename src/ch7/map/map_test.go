256b0e50fdb7285adab70e38f48fb115cb7a1c81
ChenZhen <chenzhennba@gmail.com>
2021-03-28 19:36:23 +0800

    feat: map中的值为func，在Go中func是一等公民

--- src/ch7/map/map_test.go
+++ src/ch7/map/map_test.go
@@ -36,3 +36,18 @@ func TestTravelMap(t *testing.T) {
 		t.Log(k, v)
 	}
 }
+
+func TestMapWithFuncValue(t *testing.T) {
+	m := map[int]func(op int) int {}
+	m[0] = func(op int) int {
+		return op
+	}
+	m[1] = func(op int) int {
+		return op * op
+	}
+	m[2] = func(op int) int {
+		return op * op * op
+	}
+
+	t.Log(m[0](2), m[1](2), m[2](2))
+}
