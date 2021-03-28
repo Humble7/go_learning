85ef6e91e6626f7ce7ae6c85583fcfcfe90ce901
ChenZhen <chenzhennba@gmail.com>
2021-03-29 00:52:52 +0800

    feat: 错误恢复机制

--- src/ch13/panic_recover/panic_recovery_test.go
+++ src/ch13/panic_recover/panic_recovery_test.go
@@ -19,3 +19,19 @@ func TestPanicVsExit(t *testing.T) {
 	// Exit没有调用栈，没有错误信息，不会执行defer函数
 	//os.Exit(-1)
 }
+
+func TestRecovery(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Recovered from", err)
+		}
+	}()
+
+	fmt.Println("Start")
+
+	// panic有调用栈，有错误信息，会执行defer函数
+	panic(errors.New("Something wrong"))
+
+	// Exit没有调用栈，没有错误信息，不会执行defer函数
+	//os.Exit(-1)
+}
