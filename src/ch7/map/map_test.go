7b97a518cefaf7bdebf6fb83b5882731cc9769a8
ChenZhen <chenzhennba@gmail.com>
2021-03-28 19:31:26 +0800

    feat: map 的基本使用

---
+++ src/ch7/map/map_test.go
@@ -0,0 +1,38 @@
+package _map
+
+import "testing"
+
+func TestMapInit(t *testing.T) {
+	m1 := map[int]int {1:1, 2:2, 3:3}
+	t.Logf("m1 length:%d", len(m1))
+
+	m2 := map[int]int{}
+	m2[4] = 4
+	t.Logf("m2 length:%d", len(m2))
+
+	m3 := make(map[int]int, 10)
+	t.Logf("m3 length:%d", len(m3))
+
+}
+
+func TestAccessNotExistingKey(t *testing.T) {
+	m1 := map[int]int {}
+	// key不存在会返回0
+	t.Log(m1[1])
+
+	m1[2] = 0
+	t.Log(m1[2])
+
+	if v,ok := m1[1]; ok {
+		t.Log("Key 1's value is", v)
+	} else {
+		t.Log("Key 1's value is not exists")
+	}
+}
+
+func TestTravelMap(t *testing.T) {
+	m := map[int]int {1:1, 2:2, 3:3}
+	for k,v := range m {
+		t.Log(k, v)
+	}
+}
