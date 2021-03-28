2db8ef8da5d9466e549d0a60bb61554d23bdbea5
ChenZhen <chenzhennba@gmail.com>
2021-03-28 15:56:53 +0800

    feat: 数组的比较

---
+++ src/ch4/operator_test/operator_test.go
@@ -0,0 +1,14 @@
+package operator_test
+
+import "testing"
+
+func TestCompareArray(t *testing.T) {
+	a := [...]int{1,2}
+	b := [...]int{2,1}
+	//c := [...]int{1,2,3}
+	d := [...]int{1,2}
+	t.Log(a == b)
+	// 长度不同不可比较
+	//t.Log(a == c)
+	t.Log(a == d)
+}
