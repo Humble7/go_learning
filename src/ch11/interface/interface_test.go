ce8405a618ce4ed4f56335d7266847464bf13785
ChenZhen <chenzhennba@gmail.com>
2021-03-28 22:41:05 +0800

    feat: go 接口-duck type

---
+++ src/ch11/interface/interface_test.go
@@ -0,0 +1,21 @@
+package _interface
+
+import "testing"
+
+type Programmer interface {
+	WriteHelloWorld() string
+}
+
+type GoProgrammer struct {
+
+}
+
+func (p *GoProgrammer) WriteHelloWorld() string {
+	return "fmt.Println(\"hello world\")"
+}
+
+func TestClient(t *testing.T) {
+	var p Programmer
+	p = new(GoProgrammer)
+	t.Log(p.WriteHelloWorld())
+}
