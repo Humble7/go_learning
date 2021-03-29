691603daa4df2c2a651051a9a034faab362e7f8a
ChenZhen <chenzhennba@gmail.com>
2021-03-30 01:03:28 +0800

    feat: go get

--- src/ch14/remote_package/remote_package_test.go
+++ src/ch14/remote_package/remote_package_test.go
@@ -1,1 +1,10 @@
 package remote_package
+
+import (
+	cm "github.com/easierway/concurrent_map"
+	"testing"
+)
+
+func TestRemotePackage(t *testing.T) {
+	cm.CreateConcurrentMap(9)
+}
--- src/ch14/series/my_series.go
+++ src/ch14/series/my_series.go
@@ -1,1 +1,32 @@
 package series
+
+import (
+	"errors"
+	"fmt"
+)
+
+func init() {
+	fmt.Println("init 1")
+}
+
+func init() {
+	fmt.Println("init 2")
+}
+
+func Fib(n int) ([]int, error) {
+	if n < 0 {
+		return nil, errors.New("n should be greater than or equal to 0")
+	}
+
+	ret := []int {1, 1}
+	for i := 2; i < n; i ++ {
+		ret = append(ret, ret[i - 1] + ret[i - 2])
+	}
+
+	return ret, nil
+}
+
+// 小写字母开头的方法，不能被包外访问
+func square(n int) int {
+	return n * n
+}
