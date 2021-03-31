ef494625420e906961bb0887cb8dc76b0c5f6638
ChenZhen <chenzhennba@gmail.com>
2021-04-01 00:09:45 +0800

    feat: csp 并发机制

---
+++ src/ch18/csp/csp_test.go
@@ -0,0 +1,50 @@
+package csp
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("Working on something else.")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done.")
+}
+
+func TestService(t *testing.T) {
+	t.Log(service())
+	otherTask()
+}
+
+func AsyncService() chan string {
+	// 方式一
+	retCh := make(chan string)
+
+	// 方式二
+	//retCh := make(chan string, 1)
+
+	go func() {
+		ret := service()
+		fmt.Println("return result")
+		retCh <- ret
+
+		// 如果是方式一的Chanel，下面这条语句会等到从Chanel中取出内容后才会执行
+		// 方式二会立马执行
+		fmt.Println("service existed")
+	}()
+
+	return retCh
+}
+
+func TestAsyncService(t *testing.T) {
+	retCh := AsyncService()
+	otherTask()
+	t.Log(<- retCh)
+	time.Sleep(time.Second * 1)
+}
