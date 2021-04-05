6feb9a54c000174ce4d0a1ce2eb1298f439bdd22
ChenZhen <chenzhennba@gmail.com>
2021-04-06 02:02:21 +0800

    feat: 扩展已有类型

---
+++ src/net_easy/tree/entry/entry.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net_easy/tree"
+)
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (node *myTreeNode) postorderTraversal() {
+    if node == nil || node.node == nil {
+		return
+	}
+
+	left := myTreeNode{node.node.Left}
+	left.postorderTraversal()
+	right := myTreeNode{node.node.Right}
+	right.postorderTraversal()
+	node.node.PrintValue()
+}
+
+func main() {
+	root := tree.Node{Value: 3}
+	root.PrintValue()
+	root.SetValue(2)
+	root.PrintValue()
+
+	root.Left = &tree.Node{Value: 1}
+	root.Right = &tree.Node{Value: 3}
+	fmt.Println("InorderTraverse")
+	root.InorderTraverse()
+
+	extendNode := myTreeNode{&root}
+	fmt.Println("postorderTraversal")
+	extendNode.postorderTraversal()
+
+	// 值为nil也可以方法调用
+	//var nilNode *Node
+	//nilNode.SetValue(2)
+
+
+}
